812e99823bd24a98fab030ba258f216572099ac3
ramdhani136 <ramdhaniit@gmail.com>
2022-06-22 10:09:41 +0700

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const API_URI = "http://localhost:5000/";
-const SOCKET_URI = "http://localhost:5000";
+const API_URI = "http://103.56.149.31:5000/";
+const SOCKET_URI = "http://103.56.149.31:5000";
 
 export { API_URI, SOCKET_URI };
